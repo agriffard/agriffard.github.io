178e3bacd3f91f3a703887b64e2410a0e78470fd
Antoine Griffard <agriffard@hotmail.com>
2023-05-22 16:39:55 +0200

    slugify

--- src/utils/getPostsByCategory.ts
+++ src/utils/getPostsByCategory.ts
@@ -1,7 +1,7 @@
 import { slugifyAll } from "./slugify";
-import type { MarkdownInstance } from "astro";
+import type { CollectionEntry } from "astro:content";
 
 const getPostsByCategory = (posts: CollectionEntry<"blog">[], tag: string) =>
-  posts.filter((post) => slugifyAll(post.frontmatter.categories).includes(tag));
+  posts.filter((post) => slugifyAll(post.data.categories).includes(tag));
 
 export default getPostsByCategory;
