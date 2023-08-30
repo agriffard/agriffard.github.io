0e8a0d44229936e921c97673bd7fecd8e15be200
Antoine Griffard <agriffard@hotmail.com>
2023-08-30 17:25:17 +0200

    Remove compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
-import robotsTxt from "astro-robots-txt";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import remarkPlantUML from "@akebifiky/remark-simple-plantuml";
-import { remarkReadingTime } from "./remark-plugins/remark-reading-time.mjs";
-import { remarkDiagram } from "./remark-plugins/remark-diagram.mjs";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import rehypeKatex from "rehype-katex";
+import { remarkDiagram } from "./remark-plugins/remark-diagram.mjs";
+import { remarkReadingTime } from "./remark-plugins/remark-reading-time.mjs";
+import remarkCollapse from "remark-collapse";
+import remarkPlantUML from "@akebifiky/remark-simple-plantuml";
+import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://agriffard.github.io/",
@@ -19,8 +18,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), react(), sitemap(), compress(),
-  robotsTxt(), mdx()],
+  }), mdx(), react(), robotsTxt(), sitemap()],
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [remarkReadingTime, remarkMath, remarkPlantUML, remarkDiagram, remarkToc, [remarkCollapse, {
