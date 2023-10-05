02c13169dd6abbefed0675c4577256fc0c95e1dc
Antoine Griffard <agriffard@hotmail.com>
2023-10-05 14:00:10 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import decapCmsOauth from "astro-decap-cms-oauth";
 
 export default defineConfig({
   site: "https://antoinegriffard.com/",
-  integrations: [tailwind({ applyBaseStyles: false }), react(), sitemap()],
+  integrations: [tailwind({ applyBaseStyles: false }), react(), sitemap(),decapCmsOauth()],
+  output: "server",
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, {
       test: "Table of contents"
