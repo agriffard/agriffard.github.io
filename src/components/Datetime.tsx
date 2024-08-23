f11841a87acaf56d93999e7a6467a8ad49a7d619
Antoine Griffard <agriffard@hotmail.com>
2024-08-23 12:01:15 +0200

    comment time

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -66,9 +66,9 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
   return (
     <>
       <time dateTime={myDatetime.toISOString()}>{date}</time>
-      <span aria-hidden="true"> | </span>
+      {/* <span aria-hidden="true"> | </span>
       <span className="sr-only">&nbsp;at&nbsp;</span>
-      <span className="text-nowrap">{time}</span>
+      <span className="text-nowrap">{time}</span> */}
     </>
   );
 };
