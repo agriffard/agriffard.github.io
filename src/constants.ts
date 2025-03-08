667e329326f59c00e8ee1733696aa038898d4a3c
Antoine Griffard <agriffard@hotmail.com>
2025-03-08 14:57:52 +0100

    Projects

--- src/constants.ts
+++ src/constants.ts
@@ -53,57 +53,6 @@ export const SHARE_LINKS = [
   },
 ] as const;
 
-export const PROJECTS: Project = [
-  {
-    name: "Orchard Core",
-    href: "https://github.com/OrchardCMS/OrchardCore",
-  },
-  {
-    name: "Orchard Core docs",
-    href: "https://docs.orchardcore.net",
-  },
-  {
-    name: "Orchard Core translations",
-    href: "https://github.com/OrchardCMS/OrchardCore.Translations",
-  },
-  {
-    name: "Orchard Core website",
-    href: "https://orchardcore.net",
-  },
-  {
-    name: "Try Orchard Core website",
-    href: "https://try.orchardcore.net",
-  },
-  {
-    name: "Orchard Core Admin theme",
-    href: "https://github.com/agriffard/TheAdminTheme",
-  },
-  {
-    name: "Orchard Core Bootstrap theme",
-    href: "https://github.com/agriffard/TheBootstrapTheme",
-  },
-  {
-    name: "Orchard Core ClassLess theme",
-    href: "https://github.com/agriffard/TheClassLessTheme",
-  },
-  {
-    name: "Orchard Core CookieConsent module",
-    href: "https://github.com/agriffard/CookieConsent.OrchardCore",
-  },
-  {
-    name: "Orchard Core Disqus.OrchardCore",
-    href: "https://github.com/agriffard/Disqus.OrchardCore",
-  },
-  {
-    name: "Orchard Core Prism module",
-    href: "https://github.com/agriffard/Prism.OrchardCore",
-  },
-  {
-    name: "Orchard Core Resume theme",
-    href: "https://github.com/agriffard/TheResumeTheme",
-  },
-];
-
 export const SOCIALS = [
   {
     name: "Microsoft",
