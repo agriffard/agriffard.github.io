abe06f22ac95aa085e0b3204881894d5f079d7f3
Antoine Griffard <agriffard@hotmail.com>
2025-03-08 16:10:54 +0100

    getPostsByCategory

--- src/utils/getPostsByCategory.ts
+++ src/utils/getPostsByCategory.ts
@@ -2,9 +2,9 @@ import type { CollectionEntry } from "astro:content";
 import getSortedPosts from "./getSortedPosts";
 import { slugifyAll } from "./slugify";
 
-const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
+const getPostsByCategory = (posts: CollectionEntry<"blog">[], category: string) =>
   getSortedPosts(
-    posts.filter(post => slugifyAll(post.data.tags).includes(tag))
+    posts.filter(post => slugifyAll(post.data.categories).includes(category))
   );
 
-export default getPostsByTag;
+export default getPostsByCategory;
