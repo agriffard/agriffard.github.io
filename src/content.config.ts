9ca9c7da771d8f9a6b5cc954cfd690f502f128e6
Antoine Griffard <agriffard@hotmail.com>
2025-03-09 13:34:08 +0100

    ogImage

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,12 +14,7 @@ const blog = defineCollection({
       draft: z.boolean().optional(),
       categories: z.array(z.string()).default(["others"]),
       tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
-        })
-        .or(z.string())
-        .optional(),
+      ogImage: image().or(z.string()).optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
       editPost: z
